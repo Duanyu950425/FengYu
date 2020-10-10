70cab33d2442d91a39b4f9ccf8d37f655707339a
happy <Duanyu_950425@163.com>
2020-10-11 00:00:36 +0800

    段雨：在cmd.c文件中添加代码，修改readme文件

--- cmd.c
+++ cmd.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main()
+void main01()
 {
     printf("突突真可爱\n");
     system("taskmgr.exe");
+}
+
+void main()
+{
+    std::wstring name = L"突突";
+    printf(name.c_str());
+    printf("突突不乖！\n");
+    system("pause");
 }
